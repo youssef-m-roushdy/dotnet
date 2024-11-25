9300bb2adf7b10c87143fa6dbb2858d5685341cc
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-11-25 20:39:48 +0200

    Product CRUD Operation Implementation

--- webapi-dotnet/AspNetCoreWebAPIProject/Controllers/ProductsController.cs
+++ webapi-dotnet/AspNetCoreWebAPIProject/Controllers/ProductsController.cs
@@ -23,5 +23,59 @@ namespace AspNetCoreWebAPIProject.Controllers
         {
             return Ok(_context.Products.ToList());
         }
+
+        [HttpPost]
+        public IActionResult AddNewProduct(string productName, double price)
+        {
+            var newProduct = new Product
+            {
+                ProductName = productName,
+                Price = price
+            };
+            try
+            {
+                _context.Products.Add(newProduct);
+                _context.SaveChanges();
+                return CreatedAtAction(nameof(AddNewProduct), new { id = newProduct.ProductId }, newProduct);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+            
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.ProductId == id);
+            if(product == null)
+                return BadRequest("Cannot find/Invalid Id");
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct(int id, string productName, double price)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.ProductId == id);
+            if(product == null)
+                return BadRequest("Cannot find/Invalid Id");
+            product.ProductName = productName;
+            product.Price = price;
+            _context.Products.Update(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult RemoveProduct(int id)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.ProductId == id);
+            if(product == null)
+                return BadRequest("Cannot find/Invalid Id");
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
