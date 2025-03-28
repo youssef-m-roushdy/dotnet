d02d4e226d5c2d22d23923bb56b43e0866eadd60
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2025-03-28 23:22:10 +0200

    start platform service and model creation

---
+++ dotnet-microservices/microservices-course/PlatformService/Models/Platform.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Models
+{
+    public class Platform
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Cost { get; set; }
+    }
+}
