d462515560735f3c8e15df7ad2e553d1fcd64599
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2025-03-29 21:44:52 +0200

    delete repo folder

--- dotnet-microservices/product-microservices-project/obj/Debug/net8.0/product-microservices-project.AssemblyInfo.cs
+++ dotnet-microservices/product-microservices-project/obj/Debug/net8.0/product-microservices-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("product-microservices-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f431c3faaa0d730a059aaf2018a2c61c69b23068")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d910d0a2990182bc48d509555bb9e6b7c28a3ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("product-microservices-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("product-microservices-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
