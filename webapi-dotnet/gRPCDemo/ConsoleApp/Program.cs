d9a7baae271c146f0e214ef1c77e5a4a83212198
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2025-04-16 23:12:10 +0200

    Communication with gRPC service

--- webapi-dotnet/gRPCDemo/ConsoleApp/Program.cs
+++ webapi-dotnet/gRPCDemo/ConsoleApp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using GrpcService.Protos;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5001");
+var client = new ProductProtoService.ProductProtoServiceClient(channel);
+
+var create1 = await client.CreateProductAsync(new CreateProductRequest
+{
+    Name = "Computer",
+    Description = "A workstation computer",
+    Price = 1800
+});
+Console.WriteLine(create1);
+Console.WriteLine("========");
+var create2 = await client.CreateProductAsync(new CreateProductRequest
+{
+    Name = "Airpods",
+    Description = "A aitpods for private listeninig",
+    Price = 100
+});
+Console.WriteLine(create1);
+Console.WriteLine("========");
+
+var productsList = await client.ListProductAsync(new Google.Protobuf.WellKnownTypes.Empty());
+
+foreach (var product in productsList.Products)
+{
+    Console.WriteLine($"${product.Id}: {product}");
+}
+
